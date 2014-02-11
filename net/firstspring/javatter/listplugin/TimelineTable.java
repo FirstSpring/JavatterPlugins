8cbe5e911d5d0e089c3701d40f7b1c229a1555cd
FirstSpring <firstspring1845@gmail.com>
2014-02-11 14:03:24 +0900

    普通のパネルっぽく見せたかった～完～

--- net/firstspring/javatter/listplugin/TimelineTable.java
+++ net/firstspring/javatter/listplugin/TimelineTable.java
@@ -2,25 +2,25 @@ package net.firstspring.javatter.listplugin;
 
 import java.awt.Component;
 import java.awt.Dimension;
-import java.awt.Point;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.lang.reflect.Method;
+import java.awt.event.MouseMotionListener;
 import java.util.EventObject;
 
 import javax.swing.CellRendererPane;
-import javax.swing.JPanel;
 import javax.swing.JTable;
-import javax.swing.SwingUtilities;
 import javax.swing.event.CellEditorListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellEditor;
 import javax.swing.table.TableCellRenderer;
 
-public class TimelineTable extends JTable
+public class TimelineTable extends JTable implements MouseMotionListener
 {
 	private static final long serialVersionUID = 1L;
 
-	public DefaultTableModel model = new DefaultTableModel(new Object[] {""}, 0);
+	int editRow = -1;
+
+	public DefaultTableModel model = new DefaultTableModel(new Object[]{""}, 0);
 
 	public TimelineTable()
 	{
@@ -53,7 +53,7 @@ public class TimelineTable extends JTable
 			@Override
 			public Component getTableCellEditorComponent(JTable table, Object value, boolean select, int row, int column)
 			{
-				return (Component)value;
+				return getDefaultRenderer(Object.class).getTableCellRendererComponent(table, value, select, false, row, column);
 			}
 		});
 		setModel(model);
@@ -61,25 +61,27 @@ public class TimelineTable extends JTable
 		{
 			public Component getTableCellRendererComponent(JTable table, Object value, boolean select, boolean focus, int row, int column)
 			{
-				Component c = (Component) value;
+				Component c = (Component)value;
 				c.setMinimumSize(null);
 				c.setPreferredSize(null);
 				c.setMaximumSize(null);
 				int height = c.getPreferredSize().height;
 				// 呼び出しがループするのを防ぐ
-				if (table.getRowHeight(row) != height)
+				if(table.getRowHeight(row) != height)
 				{
 					table.setRowHeight(row, c.getPreferredSize().height);
 				}
 				return c;
 			}
 		});
+		addMouseMotionListener(this);
 	}
 
 	public void addTop(Object o)
 	{
 		if(isEditing())
 		{
+			editRow = -1;
 			removeEditor();
 		}
 		model.insertRow(0, new Object[]{o});
@@ -89,20 +91,37 @@ public class TimelineTable extends JTable
 	{
 		if(isEditing())
 		{
+			editRow = -1;
 			removeEditor();
 		}
 		model.addRow(new Object[]{o});
 	}
-	
+
 	@Override
-	public void removeEditor(){
-		for (Component c : getComponents())
+	public void removeEditor()
+	{
+		for(Component c : getComponents())
 		{
-			if (!(c instanceof CellRendererPane))
+			if(!(c instanceof CellRendererPane))
 			{
 				remove(c);
 			}
 		}
 		super.removeEditor();
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e){}
+
+	@Override
+	public void mouseMoved(MouseEvent e)
+	{
+		int r = rowAtPoint(e.getPoint());
+		if(r != editRow)
+		{
+			removeEditor();
+			editRow = r;
+			editCellAt(editRow, 0);
+		}
 	}
 }
